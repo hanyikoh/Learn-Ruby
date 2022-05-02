58277c7f07b6cd2cf10836130c170e00dadf0df9
hanyikoh <hanyikoh@gmail.com>
2022-05-02 10:55:39 +0800

    feat: add comments

--- Section 2/2.19_Working_with_Numbers.rb
+++ Section 2/2.19_Working_with_Numbers.rb
@@ -3,43 +3,53 @@ puts 1 + 2
 # => nil
 
 # IRB
-puts 10 / 2
+# Integer divided by Integer will get back integer
+puts 10 / 2 
 # => 5
-puts 10 / 4
+puts 10 / 4 # The remainder is not getting printed out cause the decimal places dropped
 # => 2
-puts 10.0 / 2
+
+# Integer divided by Float will get back Float
+puts 10.0 / 2 
 # => 5.0
 puts 10 / 2.0
 # => 5.0
-puts 10 / 4.to_f
+
+# Integer Conversion to Float
+puts 10 / 4.to_f # = 10 / 4.0
 # => 2.5
-puts (10 / 4).to_f
+puts (10 / 4).to_f # = ( 10 / 4 ).to_f, the formula inside parenthesis will be execiuted first
 # => 2.0
 
+# Mathematical Operation with Variables
 x = 5
 y = 10
 puts y/x
+# => 2
 
+# string multiply with numbers
 puts "I as a line"
 puts "-" * 20
 puts "I as a diff line after a divider"
 20.times{print "-"}
 
-x = "5".to_i
+# More conversion practice
+x = "5".to_i # string convert to integer and store in a variable
 # => 5
-puts x.to_f
+puts x.to_f # integer convert to float
 # => 5.0
-y = "10".to_f
+y = "10".to_f # float convert to float and store in a variable
 puts x * y
 # => 50.0
-puts "hello".to_i
+puts "hello".to_i # strings convert to integer ( will get back zero for non-numbers string value )
 # => 0
-puts "hello".to_f
+puts "hello".to_f # strings convert to float ( will get back 0.0 for non-numbers string value )
 # => 0.0
 x = "hello"
 puts x.to_i
 # => 0
 
+# Calculator Exercise
 puts "Simple Calculator"
 25.times{print"-"}
 puts
@@ -48,5 +58,5 @@ num_1 = gets.chomp
 puts "Enter the second number" 
 num_2 = gets.chomp
 puts "The first number multiplied by the second number is #{num_1.to_i * num_2.to_i}"
-puts "The modulus between first number and the second number is #{num_1.to_i % num_2.to_i}"
+puts "The modulus between first number and the second number is #{num_1.to_i % num_2.to_i}" # Modulus
 
