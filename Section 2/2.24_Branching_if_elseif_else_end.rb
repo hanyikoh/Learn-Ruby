2827bea565c3561b0bf71617f73bc3b28331060f
hanyikoh <hanyikoh@gmail.com>
2022-05-05 13:29:49 +0800

    feat: add more practice and calculator branching exercise

--- Section 2/2.24_Branching_if_elseif_else_end.rb
+++ Section 2/2.24_Branching_if_elseif_else_end.rb
@@ -75,3 +75,36 @@ else
     puts "This evaluated to false"
 end 
 # => "This evaluated to true"
+
+
+name = "Mashrur"
+if name == "Mashrur"
+    puts "Welcome to the program, Mashrur"
+elsif name == "Jack"
+    puts "welcome to the program, Jack"
+elsif name == "Evgeny"
+    puts "welcome to the program, Evgeny"
+else
+    puts "Welcome to the program, user"
+end
+
+puts "Simple Calculator"
+20.times{print "-"}
+puts
+puts "Please enter your first number"
+first_number = gets.chomp
+puts "Please enter your second number"
+second_number = gets.chomp
+puts "what do you want to do?"
+puts "Enter 1 for multiply, 2 for addition, 3 for subtraction"
+
+user_entry = gets.chomp
+if user_entry == "1"
+    puts "You have chosen to multiply"
+elsif user_entry == "2"
+    puts "You have chosen to add"
+elsif user_entry == "3"
+    puts "You have chosen to subtract"
+else 
+    puts "Invalid entry"
+end
