df37018518ff0ecf1629e864f4fc115db5bcf901
hanyikoh <hanyikoh@gmail.com>
2022-07-18 21:49:34 +0800

    fix: fix indentation

--- Section 2/2.33_Ruby_Style_Guide.rb
+++ Section 2/2.33_Ruby_Style_Guide.rb
@@ -2,11 +2,11 @@
 # Ruby file names: this_is_snake_case.rb
 
 def my_name_is
-    # And my code is here
-    # Ruby use two spaces as one indent
-  end
+  # And my code is here
+  # Ruby use two spaces as one indent
+end
   
-  class SomeClass # The class name is capitalize
+class SomeClass # The class name is capitalize
   
-  # More guide can be found at:
-  # https://github.com/rubocop/ruby-style-guide
+# More guide can be found at:
+# https://github.com/rubocop/ruby-style-guide
