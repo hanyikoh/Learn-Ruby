c80f7d66ea2a84d281dc982dcb3f50ac16a2bbfa
hanyikoh <hanyikoh@gmail.com>
2022-08-07 01:26:13 +0800

    fix: indentation fix

--- Section 2/2.35_Area_Code_Dictionary_Homework.rb
+++ Section 2/2.35_Area_Code_Dictionary_Homework.rb
@@ -11,31 +11,31 @@ dial_book = {
     "lancaster" => "717"
   }
    
-  # Get city names from the hash
-  def get_city_names(somehash)
-    puts somehash.keys
-  end
-   
-  # Get area code based on given hash and key
-  def get_area_code(somehash, key)
+# Get city names from the hash
+def get_city_names(somehash)
+  puts somehash.keys
+end
+  
+# Get area code based on given hash and key
+def get_area_code(somehash, key)
   return somehash[key];
+end
+  
+# Execution flow
+loop do
+  puts "Do you want to lookup an area code based on a city name?(Y/N)"
+  answer = gets.chomp.downcase # Downcasing the input so that it accepts both Y and y
+  break if answer != "y"
+  puts "Which city do you want the area code for?"
+  puts get_city_names(dial_book)
+  puts "Enter your selection"
+  prompt = gets.chomp
+  if dial_book.include?(prompt)
+    puts "The area code for #{prompt} is #{get_area_code(dial_book, prompt)}"
+  else
+    puts "You entered a city name not in the dictionary"
   end
-   
-  # Execution flow
-  loop do
-    puts "Do you want to lookup an area code based on a city name?(Y/N)"
-    answer = gets.chomp.downcase # Downcasing the input so that it accepts both Y and y
-    break if answer != "y"
-    puts "Which city do you want the area code for?"
-    puts get_city_names(dial_book)
-    puts "Enter your selection"
-    prompt = gets.chomp
-    if dial_book.include?(prompt)
-      puts "The area code for #{prompt} is #{get_area_code(dial_book, prompt)}"
-    else
-      puts "You entered a city name not in the dictionary"
-    end
-  end
+end
 
   
 # => Do you want to lookup an area code based on a city name?(Y/N)
