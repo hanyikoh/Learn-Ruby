e159df92542fdccaacb726c4f7d1eb4577bfa484
hanyikoh <hanyikoh@gmail.com>
2022-11-16 22:15:16 +0800

    feat: put functions intp module

--- Section 2/2.48_Mixins_3_Modules.rb
+++ Section 2/2.48_Mixins_3_Modules.rb
@@ -1,37 +1,38 @@
-# require 'bundler/inline'
+require 'bundler/inline'
  
-# gemfile true do
-#  source 'http://rubygems.org'
-#  gem 'bcrypt'
-# end
-
-# $LOAD_PATH << "."
-# require 'crud' 
-
-require 'bcrypt'
- 
-users = [
-  { username: "mashrur", password: "password1" },
-  { username: "jack", password: "password2" },
-  { username: "arya", password: "password3" },
-  { username: "jonshow", password: "password4" },
-  { username: "heisenberg", password: "password5" }
-]
- 
-def create_hash_digest(password)
-  BCrypt::Password.create(password)
+gemfile true do
+ source 'http://rubygems.org'
+ gem 'bcrypt'
 end
  
-def verify_hash_digest(password)
-  BCrypt::Password.new(password)
-end
+module Crud
+  require 'bcrypt'
+  puts "Module CRUD activated"
  
-def create_secure_users(list_of_users)
-  list_of_users.each do |user_record|
-    user_record[:password] = create_hash_digest(user_record[:password])
+  # When you call a method with its class name like this, 
+  # it's called a class method, and you can also use self.creaate_secure_users here, 
+  # instead of Crud.create_secure_users
+  def Crud.create_hash_digest(password)
+    BCrypt::Password.create(password)
   end
-
-  list_of_users
+ 
+  def Crud.verify_hash_digest(password)
+    BCrypt::Password.new(password)
+  end
+ 
+  def Crud.create_secure_users(list_of_users)
+    list_of_users.each do |user_record|
+      user_record[:password] = create_hash_digest(user_record[:password])
+    end
+    list_of_users
+  end
+ 
+  def Crud.authenticate_user(username, password, list_of_users)
+    list_of_users.each do |user_record|
+      if user_record[:username] == username && verify_hash_digest(user_record[:password]) == password
+        return user_record
+      end
+    end
+    "Credentials were not correct"
+  end
 end
-
-puts create_secure_users(users)
