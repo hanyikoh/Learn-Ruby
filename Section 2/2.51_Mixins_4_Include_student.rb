e868953aa3d505cc1aaeca2832854db1e71f671a
hanyikoh <hanyikoh@gmail.com>
2022-11-17 20:17:38 +0800

    feat: tidy up code

--- Section 2/2.51_Mixins_4_Include_student.rb
+++ Section 2/2.51_Mixins_4_Include_student.rb
@@ -13,16 +13,13 @@ class Student
   end
  
   def to_s
-    "First name: #{@first_name}, Last name: #{@last_name}, Username: #{@username},
-                  email address: #{@email}"
+    "First name: #{@first_name}, Last name: #{@last_name}, Username: #{@username}, email address: #{@email}"
   end
  
 end
  
-mashrur = Student.new("Mashrur", "Hossain", "mashrur1", "mashrur@example.com",
-                      "password1")
-john = Student.new("John", "Doe", "john1", "john1@example.com",
-                      "password2")
+mashrur = Student.new("Mashrur", "Hossain", "mashrur1", "mashrur@example.com","password1")
+john = Student.new("John", "Doe", "john1", "john1@example.com", "password2")
  
 hashed_password = mashrur.create_hash_digest(mashrur.password)
  
