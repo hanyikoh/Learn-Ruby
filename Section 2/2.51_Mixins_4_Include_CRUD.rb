96379e931fc5cd731f7af0ebbab578146568513a
hanyikoh <hanyikoh@gmail.com>
2022-11-17 19:31:18 +0800

    feat: remove self notation from class methods

--- Section 2/2.51_Mixins_4_Include_CRUD.rb
+++ Section 2/2.51_Mixins_4_Include_CRUD.rb
@@ -8,26 +8,23 @@ end
 module Crud
   require 'bcrypt'
   puts "Module CRUD activated"
- 
-  # When you call a method with its class name like this, 
-  # it's called a class method, and you can also use self.creaate_secure_users here, 
-  # instead of Crud.create_secure_users
-  def Crud.create_hash_digest(password)
+
+  def create_hash_digest(password)
     BCrypt::Password.create(password)
   end
  
-  def Crud.verify_hash_digest(password)
+  def verify_hash_digest(password)
     BCrypt::Password.new(password)
   end
  
-  def Crud.create_secure_users(list_of_users)
+  def create_secure_users(list_of_users)
     list_of_users.each do |user_record|
       user_record[:password] = create_hash_digest(user_record[:password])
     end
     list_of_users
   end
  
-  def Crud.authenticate_user(username, password, list_of_users)
+  def authenticate_user(username, password, list_of_users)
     list_of_users.each do |user_record|
       if user_record[:username] == username && verify_hash_digest(user_record[:password]) == password
         return user_record
